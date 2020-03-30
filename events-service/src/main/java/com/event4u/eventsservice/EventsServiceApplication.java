7243a2ad75d4b9f1c051f58a939e1ed97b4aac0d
Dzenana Sabovic <37186853+dsabovic1@users.noreply.github.com>
2020-03-30 16:07:20 +0200

    Eureka changes (#12)

--- events-service/src/main/java/com/event4u/eventsservice/EventsServiceApplication.java
+++ events-service/src/main/java/com/event4u/eventsservice/EventsServiceApplication.java
@@ -12,18 +12,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 
 import java.awt.*;
 import java.time.LocalDate;
 
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class EventsServiceApplication {
 	private static final Logger log =
@@ -34,6 +37,11 @@ public class EventsServiceApplication {
 		SpringApplication.run(EventsServiceApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	@Bean
 	public CommandLineRunner demo(CategoryRepository cRepository, UserRepository uRepository, LocationRepository lRepository, EventRepository eRepository, EventUserRepository euRepository) {
